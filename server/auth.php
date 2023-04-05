ee72b669b847cb77edb11b4c8839a2088a2ee97f
Adeolu David <adeolusegun99@gmail.com>
2023-04-05 13:22:05 +0100

    added php file

---
+++ server/auth.php
@@ -0,0 +1,36 @@
+<?php
+// Set your API key here
+$api_key = 'your_api_key';
+
+// Set the API endpoint URL
+$endpoint_url = 'https://api.spacexdata.com/v4/capsules';
+
+// Create a new cURL session
+$curl = curl_init();
+
+// Set the cURL options
+curl_setopt_array($curl, array(
+  CURLOPT_URL => $endpoint_url,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_HTTPHEADER => array(
+    'Authorization: Bearer ' . $api_key,
+    'Content-Type: application/json'
+  )
+));
+
+// Send the request and get the response
+$response = curl_exec($curl);
+
+// Check for errors
+if (curl_errno($curl)) {
+  $error_msg = curl_error($curl);
+  // Handle the error
+} else {
+  // Handle the response
+  $json_data = json_decode($response, true);
+  // Do something with the data
+}
+
+// Close the cURL session
+curl_close($curl);
+?>
--- src/pages/Rocket.js
+++ src/pages/Rocket.js
@@ -9,20 +9,19 @@ import Pagination from "../components/pagination";
 
 const slidesRocket = [
   {
-    img: "https://www.nasa.gov/sites/default/files/styles/image_card_4x3_ratio/public/thumbnails/image/crs-22_solar_arrays.jpg",
+    img: "https://media.gettyimages.com/id/1227885776/photo/in-this-handout-image-provided-by-nasa-the-spacex-crew-dragon-capsule-spacecraft-with-nasa.jpg?s=612x612&w=0&k=20&c=Lb8f0GJIxki395WmooWgM534Sj12byFZJ0Nd044wZHM=",
     id: "1",
   },
-
   {
-    img: "https://www.nasa.gov/sites/default/files/styles/image_card_4x3_ratio/public/thumbnails/image/iss062e087334.jpg",
+    img: "https://media.gettyimages.com/id/1216299222/photo/us-president-donald-trump-speaks-near-a-spacex-crew-dragon-capsule-at-a-press-briefing-after.jpg?s=612x612&w=0&k=20&c=9n_YViv0H_ZjcNiKxUlSR-233bX_upob1-VIYMB-d6U=",
     id: "2",
   },
   {
-    img: "https://www.nasa.gov/sites/default/files/styles/image_card_4x3_ratio/public/thumbnails/image/iss061e070168.jpg",
+    img: "https://media.gettyimages.com/id/1074353714/photo/bloomberg-best-of-the-year-2018-an-attendee-takes-pictures-of-a-mock-up-of-the-crew-dragon.jpg?s=612x612&w=0&k=20&c=xfbZMq8OQZ_ihfpJoFblzlB6WNCo8iX51kx6ipYr8A8=",
     id: "3",
   },
   {
-    img: "https://www.nasa.gov/sites/default/files/styles/image_card_4x3_ratio/public/thumbnails/image/iss061e069087.jpg",
+    img: "https://media.gettyimages.com/id/494553139/photo/the-interior-of-spacexs-new-seven-seat-dragon-v2-spacecraft-the-companys-next-generation.jpg?s=612x612&w=0&k=20&c=HEmP6qlxptgIHYkKb3I0qWoA-sxyL4RvoROifw3oMT8=",
     id: "4",
   },
 ];
